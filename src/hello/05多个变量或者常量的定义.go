4c786ea70f92a0ab3fa4b6cba6b68874a64d27e4
ChengqunYang <1521410544@qq.com>
2019-11-29 14:11:00 +0800

    go语言基本语法练习

--- src/hello/05多个变量或者常量的定义.go
+++ src/hello/05多个变量或者常量的定义.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
+	var a int
+	a = 20
+	fmt.Println(a)
+
+	var b = 15
+	fmt.Println(b)
+
+	c := 10
+	fmt.Println(c)
+
+	d, e := 30, 40
+	fmt.Println(d, e)
+
+	//不同类型变量的定义
+	var f int = 1
+	var g float64 = 2.0
+	fmt.Println(f, g)
 
 }
