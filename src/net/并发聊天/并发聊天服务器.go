51297c0c3a69c603ef8305c67bf8644850d9c89f
ChengqunYang <1521410544@qq.com>
2020-01-08 14:49:06 +0800

    网络编程

--- src/net/并发聊天/并发聊天服务器.go
+++ src/net/并发聊天/并发聊天服务器.go
@@ -1,8 +1,11 @@
 package main
 
+//主要功能,转发广播
 import (
 	"fmt"
 	"net"
+	"strings"
+	"time"
 )
 
 func HandleConn(conn net.Conn) {
@@ -19,9 +22,62 @@ func HandleConn(conn net.Conn) {
 	go WriteMsgToClint(cli, conn)
 
 	//广播某个人在线
-	message <- "{" + cli.Addr + "}" + cli.Name + ":  longin"
-	for true {
+	//message <- "{" + cli.Addr + "}" + cli.Name + ":  longin"
+	message <- MakeMsg(cli, "login")
+	//提示我是谁
+	cli.C <- MakeMsg(cli, "I am here")
 
+	hasData := make(chan bool) //定义一个对方是否有数据发送的通道
+	isQuit := make(chan bool)  //定义一个接受用户退出的通道
+
+	//新建一个协程,接受用户发送过来的数据
+	go func() {
+		buf := make([]byte, 2048)
+		for true {
+			n, err := conn.Read(buf)
+			if n == 0 { //对方断开,或者出问题
+				isQuit <- true
+				fmt.Println("conn.Read err = ", err)
+				return
+			}
+
+			msg := string(buf[:n-1])
+			//查看当前在线的用户都有哪些
+			if len(msg) == 3 && msg == "who" {
+				//遍历map,给当前用户发送所有在线成员
+				conn.Write([]byte("user list:"))
+				for _, user := range onlineMap {
+					msg = user.Addr + ":" + user.Name
+					conn.Write([]byte(msg))
+				}
+			} else if len(msg) >= 8 && msg[:6] == "rename" {
+				//给用户重命名
+				name := strings.Split(msg, "|")[1]
+				cli.Name = name
+				onlineMap[cliAddr] = cli
+				conn.Write([]byte("rename ok"))
+
+			} else {
+				//转发此内容
+				message <- MakeMsg(cli, msg)
+			}
+			hasData <- true //能进入,即表示有数据写入
+		}
+	}()
+
+	for true {
+		//通过select检测channel的流动
+		select {
+		case <-isQuit:
+			delete(onlineMap, cliAddr)           //将当前用户从map中移除
+			message <- MakeMsg(cli, "login out") //广播某个用户下线
+			return
+		case <-hasData:
+		case <-time.After(60 * time.Second): //60秒后给管道中写入数据
+			delete(onlineMap, cliAddr)                    //将当前用户从map中移除
+			message <- MakeMsg(cli, "time out leave out") //广播某个用户超时并断开连接
+			return
+		}
 	}
 }
 
@@ -41,6 +97,10 @@ func Manager() {
 			client.C <- msg
 		}
 	}
+}
+func MakeMsg(cli Client, msg string) (buf string) {
+	buf = "{" + cli.Addr + "}" + cli.Name + ":" + msg
+	return
 }
 
 type Client struct {
